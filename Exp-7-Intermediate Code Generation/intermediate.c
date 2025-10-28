57598496c524ef076834c113dd74807505cd19c4
Athira Francis <162030778+athirafrancis05@users.noreply.github.com>
2025-10-28 11:02:58 +0530

    Update intermediate.c

--- Exp-7-Intermediate Code Generation/intermediate.c
+++ Exp-7-Intermediate Code Generation/intermediate.c
@@ -1,53 +1,52 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 
-#define MAX 100
+char st[100]; int top=-1;
 
-char stack[MAX];
-int top = -1;
+// stack ops
+void push(char c){ st[++top]=c; }
+char pop(){ return st[top--]; }
 
-void push(char c){ stack[++top] = c; }
-char pop(){ return top >= 0 ? stack[top--] : 0; }
+// precedence
 int prec(char c){ return (c=='+'||c=='-')?1:(c=='*'||c=='/')?2:0; }
 
-void infixToPostfix(char *infix, char *postfix){
-    int k=0;
-    for(int i=0; infix[i]; i++){
-        char c = infix[i];
-        if(isalnum(c)) postfix[k++] = c;
+// ---- infix to postfix ----
+void infixToPostfix(char in[], char post[]) {
+    int k=0; char c;
+    for(int i=0; in[i]; i++){
+        c=in[i];
+        if(isalnum(c)) post[k++]=c;
         else if(c=='(') push(c);
-        else if(c==')'){ while(top>=0 && stack[top]!='(') postfix[k++]=pop(); pop(); }
-        else{ while(top>=0 && prec(stack[top])>=prec(c)) postfix[k++]=pop(); push(c); }
+        else if(c==')'){ while(st[top]!='(') post[k++]=pop(); pop(); }
+        else{ while(top!=-1 && prec(st[top])>=prec(c)) post[k++]=pop(); push(c); }
     }
-    while(top>=0) postfix[k++] = pop();
-    postfix[k]='\0';
+    while(top!=-1) post[k++]=pop();
+    post[k]='\0';
 }
 
-void generateIC(char *postfix){
-    char tempStack[MAX][10];
-    int tempTop=-1, t=1;
-    for(int i=0; postfix[i]; i++){
-        char c = postfix[i];
-        if(isalnum(c)){
-            char s[2]={c,'\0'};
-            strcpy(tempStack[++tempTop], s);
-        } else {
-            char *b=tempStack[tempTop--], *a=tempStack[tempTop--], temp[10];
-            sprintf(temp,"t%d",t++);
-            printf("%s = %s %c %s\n", temp, a, c, b);
-            strcpy(tempStack[++tempTop], temp);
+// ---- postfix to intermediate code ----
+void genCode(char post[]) {
+    top=-1; int t=1;
+    for(int i=0; post[i]; i++){
+        char c=post[i];
+        if(isalnum(c)) push(c);
+        else{
+            char b=pop(), a=pop();
+            printf("t%d = %c %c %c\n", t, a, c, b);
+            push('A'+t-1);
+            t++;
         }
     }
 }
 
+// ---- main ----
 int main(){
-    char infix[MAX], postfix[MAX];
-    printf("Enter infix expression: ");
-    fgets(infix,sizeof(infix),stdin);
-    infix[strcspn(infix,"\n")]=0;
-    infixToPostfix(infix,postfix);
-    printf("Postfix Expression: %s\n", postfix);
-    generateIC(postfix);
-    return 0;
+    char infix[100], postfix[100];
+    printf("Enter infix: ");
+    scanf("%s", infix);
+    infixToPostfix(infix, postfix);
+    printf("Postfix: %s\n", postfix);
+    printf("Intermediate Code:\n");
+    genCode(postfix);
 }
+
